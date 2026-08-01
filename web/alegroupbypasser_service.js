6cceb4ca1018ef0b52f36a1d052fadc1b7e2237a
ganbozza <ganbozza@gmail.com>
2026-08-02 01:36:49 +0800

    Refactor group state check and add getGroupNodes method

--- web/alegroupbypasser_service.js
+++ web/alegroupbypasser_service.js
@@ -95,11 +95,13 @@ class AleGroupBypasserService {
         }
     
         if (group_collections.get(key).value === MODE_BYPASS) { // ignore if group already in active state
-            this.group_collections.get(key).value = (Array.from(group._children).filter((c) => c instanceof LGraphNode).some((n) => n.mode === MODE_ACTIVE)) ? MODE_ACTIVE : MODE_BYPASS;
+            this.group_collections.get(key).value = (getGroupNodes(group).some((n) => n.mode === MODE_ACTIVE)) ? MODE_ACTIVE : MODE_BYPASS;
         }
         
     }
-    
+    getGroupNodes(group) {
+        return Array.from(group._children).filter((c) => c instanceof LGraphNode);
+    }
     registerNode(node) {
         this.nodes.add(node);
         console.log("Adding node...");
