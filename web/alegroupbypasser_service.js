d7fbdeac1e0ab3b91bc1bba9eb8ad296b4f303bd
ganbozza <ganbozza@gmail.com>
2026-08-02 01:30:59 +0800

    Update alegroupbypasser_service.js

--- web/alegroupbypasser_service.js
+++ web/alegroupbypasser_service.js
@@ -85,20 +85,19 @@ class AleGroupBypasserService {
         if(!title) { 
           return; 
         }
-        /*
         const key = title.toLowerCase();
-        if(!group_collections.has(key)) {
-            group_collections.set(key, {
+        if(!this.group_collections.has(key)) {
+            this.group_collections.set(key, {
                 key,
                 title,
                 value = MODE_BYPASS; 
             }); 
         }
     
-        if (group_collections.get(key).value===MODE_BYPASS) { // ignore if group already in active state
-            group_collections.get(key).value = Array.from(group._children).filter((c) => c instanceof LGraphNode).some((n) => n.mode === MODE_ACTIVE) ? MODE_ACTIVE : MODE_BYPASS;
+        if (this.group_collections.get(key).value===MODE_BYPASS) { // ignore if group already in active state
+            this.group_collections.get(key).value = Array.from(group._children).filter((c) => c instanceof LGraphNode).some((n) => n.mode === MODE_ACTIVE) ? MODE_ACTIVE : MODE_BYPASS;
         }
-        */
+        
     }
     
     registerNode(node) {
