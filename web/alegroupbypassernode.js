9790f24c98e2a73b13240e8323ec9af3dcd03376
ganbozza <ganbozza@gmail.com>
2026-08-01 14:40:36 +0800

    Update alegroupbypassernode.js

--- web/alegroupbypassernode.js
+++ web/alegroupbypassernode.js
@@ -1,5 +1,5 @@
 import { app } from "../../scripts/app.js";
-//import { ALEGROUPBYPASSER_SERVICE } from "./alegroupbypasser_service.js";
+import { ALEGROUPBYPASSER_SERVICE } from "./alegroupbypasser_service.js";
 
 function bindNode(node) {
   if (node.__groupBypasserBound) {
@@ -17,11 +17,10 @@ app.registerExtension({
           console.log("OK: "+String(nodeData?.name || ""));
           return;
         }
-    console.log("BBBB");
         const originalOnNodeCreated = nodeType.prototype.onNodeCreated;
         nodeType.prototype.onNodeCreated = function () {
           const result = originalOnNodeCreated?.apply(this, arguments);
-          //ALEGROUPBYPASSER_SERVICE.init();
+          ALEGROUPBYPASSER_SERVICE.init();
           bindNode(this);
           return result;
         }
