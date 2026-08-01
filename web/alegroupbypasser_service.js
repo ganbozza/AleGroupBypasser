cd7f4e8ed7537d9be3499e0f1a6caa445ded6e15
ganbozza <ganbozza@gmail.com>
2026-08-02 01:38:33 +0800

    Update alegroupbypasser_service.js

--- web/alegroupbypasser_service.js
+++ web/alegroupbypasser_service.js
@@ -95,7 +95,7 @@ class AleGroupBypasserService {
         }
     
         if (group_collections.get(key).value === MODE_BYPASS) { // ignore if group already in active state
-            this.group_collections.get(key).value = (getGroupNodes(group).some((n) => n.mode === MODE_ACTIVE)) ? MODE_ACTIVE : MODE_BYPASS;
+            //this.group_collections.get(key).value = (getGroupNodes(group).some((n) => n.mode === MODE_ACTIVE)) ? MODE_ACTIVE : MODE_BYPASS;
         }
         
     }
