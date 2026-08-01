cb32357be4661d63f1a426628d46a0788cc4988a
ganbozza <ganbozza@gmail.com>
2026-08-02 00:23:52 +0800

    Update alegroupbypassernode.js

--- web/alegroupbypassernode.js
+++ web/alegroupbypassernode.js
@@ -7,12 +7,7 @@ function bindNode(node) {
   }
   node.__groupBypasserBound = true;
 
-  const originalOnRemoved = node.onRemoved;
-  node.onRemoved = function () {
-    // Clean up service references safely when deleted from canvas
-    ALEGROUPBYPASSER_SERVICE.unregisterNode(this);
-    return originalOnRemoved?.apply(this, arguments);
-  };
+
 }
 
 app.registerExtension({
@@ -28,52 +23,9 @@ app.registerExtension({
           const result = originalOnNodeCreated?.apply(this, arguments);
           bindNode(this);
           ALEGROUPBYPASSER_SERVICE.init();
-          ALEGROUPBYPASSER_SERVICE.registerNode(this);
+          //ALEGROUPBYPASSER_SERVICE.registerNode(this);
           return result;
         }
-
-      /*
-    init() {
-        ALEGROUPBYPASSER_SERVICE.init();
-    },
-
-    registerCustomNodes() {
-        class AleGroupBypasserControllerNode {
-            constructor() {
-                this.title = "Group Toggle Node";
-                this.size =;
-                this.shape = LiteGraph.BOX_SHAPE;
-
-                // Add a dropdown widget matching LiteGraph mode constants:
-                // LiteGraph.ALWAYS = 0 (Active)
-                // LiteGraph.NEVER = 2  (Muted)
-                // LiteGraph.BYPASS = 4 (Bypassed)
-                this.modeWidget = this.addWidget(
-                    "combo", 
-                    "Group State", 
-                    0, 
-                    (value) => { this.onStateChanged(value); }, 
-                    { values:, labels: ["Active", "Mute", "Bypass"] }
-                );
-
-                // Instantly register with our global manager
-                ALEGROUPBYPASSER_SERVICE.registerController(this);
-            }
-
-            onStateChanged(newValue) {
-                // Refresh the engine immediately when user changes the dropdown
-                ALEGROUPBYPASSER_SERVICE.updateAllGroupsState();
-            }
-
-            onRemoved() {
-                // Clean up service references safely when deleted from canvas
-                ALEGROUPBYPASSER_SERVICE.unregisterController(this);
-            }
-        }
-
-        LiteGraph.registerNodeType("AleGroupBypasserControllerNode", AleGroupBypasserControllerNode);
-    }
-    */
     },
   loadedGraphNode(node) {
     console.log("AAAAA");
