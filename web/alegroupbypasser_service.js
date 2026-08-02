b03fa712cca1045101bce8c4da577950f0cfe1f2
ganbozza <ganbozza@gmail.com>
2026-08-02 16:45:05 +0800

    Update alegroupbypasser_service.js

--- web/alegroupbypasser_service.js
+++ web/alegroupbypasser_service.js
@@ -56,7 +56,7 @@ class AleGroupBypasserService {
           for (const node of self.nodes) {
             if(node.widgets) {
               for(const widget of node.widgets) {
-                const group = findGroupInCollectionByKey(normalizedTitle(widget.name));
+                const group = self.findGroupInCollectionByKey(normalizedTitle(widget.name));
                 if(group)
                 {
                   if(widget.value!==group.value) {
