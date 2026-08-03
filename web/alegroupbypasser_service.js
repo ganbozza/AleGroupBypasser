a7c194f8a8a695e8fb065f9d1fc3906c9f9dcc05
ganbozza <ganbozza@gmail.com>
2026-08-03 13:44:52 +0800

    Comment out unused slot widget value logic
    Comment out unused code related to slot widget value assignment.

--- web/alegroupbypasser_service.js
+++ web/alegroupbypasser_service.js
@@ -63,6 +63,7 @@ class AleGroupBypasserService {
                   if(widget.value!==group_toggle_value) {
                     console.log("Changing value for "+widget.name);
                   }
+                  /*
                   if(widget._inputslot_origin_id) {
                     const slotNode = app.graph.getNodeById(widget._inputslot_origin_id);
                     const slotWidget = slotNode.widgets?.find(w => w.type === "toggle" || w.name === "value") || slotNode.widgets?.[0];
@@ -70,8 +71,9 @@ class AleGroupBypasserService {
                       slotWidget.value =  group_toggle_value;
                     }
                   } else {
+                  */
                     widget.value = group_toggle_value;
-                  }
+                  //}
                 }
               }
             }
