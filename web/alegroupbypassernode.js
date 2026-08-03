427c3d02c54e707fe2b176245ca9e2cd7b354389
ganbozza <ganbozza@gmail.com>
2026-08-03 09:41:24 +0800

    Update alegroupbypassernode.js

--- web/alegroupbypassernode.js
+++ web/alegroupbypassernode.js
@@ -41,20 +41,22 @@ function refreshWidgets(node) {
       updated = true;
     } 
   }
-  for(const widget of node.widgets) {
-    if(widget._inputslot_origin_id) {
-      const upstreamNode = app.graph.getNodeById(link_info.origin_id);
-      const upstreamWidget = upstreamNode.widgets?.[0] || upstreamNode.widgets?.find(w => w.type === "toggle" || w.name === "value");
-       if (upstreamWidget && typeof upstreamWidget.value !== undefined) {
-         const upstreamValue = upstreamWidget.value;
-         if(widget.value!=upstreamWidget.value) {
-           widget.value=upstreamWidget.value;
-           updated = true;
-          if (typeof widget.callback === "function") {
-              localWidget.callback(upstreamValue);
-          }
+  if(node.widgets) {
+    for(const widget of node.widgets) {
+      if(widget._inputslot_origin_id) {
+        const upstreamNode = app.graph.getNodeById(link_info.origin_id);
+        const upstreamWidget = upstreamNode.widgets?.[0] || upstreamNode.widgets?.find(w => w.type === "toggle" || w.name === "value");
+         if (upstreamWidget && typeof upstreamWidget.value !== undefined) {
+           const upstreamValue = upstreamWidget.value;
+           if(widget.value!=upstreamWidget.value) {
+             widget.value=upstreamWidget.value;
+             updated = true;
+            if (typeof widget.callback === "function") {
+                localWidget.callback(upstreamValue);
+            }
+           }
          }
-       }
+      }
     }
   }
   if(updated) {
