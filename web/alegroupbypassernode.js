1359c2887c8eb504bf24da6b4bd2ff52b1e62c22
ganbozza <ganbozza@gmail.com>
2026-08-03 11:04:42 +0800

    Update alegroupbypassernode.js

--- web/alegroupbypassernode.js
+++ web/alegroupbypassernode.js
@@ -201,8 +201,8 @@ app.registerExtension({
         const originalOnConfigure = nodeType.prototype.onConfigure;
         nodeType.prototype.onConfigure = function (info) {
           const result = originalOnConfigure?.apply(this, arguments);
-
-          info.inputs[0].widget._inputslot_origin_id = app.graph.links[info.inputs[0].link].origin_id;
+          if(info.inputs[0].link)
+            info.inputs[0].widget._inputslot_origin_id = app.graph.links[info.inputs[0].link].origin_id;
           /*
           // Read how many inputs existed when the workflow was saved
           if (info.inputs && info.inputs.length > 0) {
