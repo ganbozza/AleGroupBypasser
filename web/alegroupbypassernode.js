10b28da5fdd8be1c5cd8d6c57dac4a9fea15e2d7
ganbozza <ganbozza@gmail.com>
2026-08-03 12:01:17 +0800

    Correct widget assignment for inputs in onConfigure
    Fix input widget assignment in onConfigure method.

--- web/alegroupbypassernode.js
+++ web/alegroupbypassernode.js
@@ -194,7 +194,7 @@ app.registerExtension({
           const result = originalOnConfigure?.apply(this, arguments);
           
           for(let i=0;i<info.inputs.length;i++) {
-              this.addInput(info.inputs[i].name, info.inputs[i].type);
+              //this.addInput(info.inputs[i].name, info.inputs[i].type);
               const boolWidget = this.addWidget(
                 "toggle",
                 info.inputs[i].name,
@@ -208,7 +208,7 @@ app.registerExtension({
                 },
                 { serialize: true }
               );
-              this.inputs[this.inputs.length - 1].widget = boolWidget;
+              this.inputs[i].widget = boolWidget;
             if(info.inputs[i].link) {
               //boolWidget._inputslot_origin_id = app.graph.links[info.inputs[i].link].origin_id;
             }
