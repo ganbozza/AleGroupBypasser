071cccfb068166ce9546dc86ef223e1a1d67b3dd
ganbozza <ganbozza@gmail.com>
2026-08-04 02:24:40 +0800

    Update alegroupbypassernode.js

--- web/alegroupbypassernode.js
+++ web/alegroupbypassernode.js
@@ -280,7 +280,7 @@ app.registerExtension({
             }
             //this.inputs[i].widget = boolWidget;
             //this.inputs[i].widget = JSON.parse(JSON.stringify(boolWidget, (key, value) => key === '_node' ? undefined : value));
-            this.inputs[i].widget = {{ name : val.title, _ref_hash : boolWidget._ref_hash };
+            this.inputs[i].widget = {{ name : info.inputs[i].name, _ref_hash : boolWidget._ref_hash };
             //this.inputs[i].widget.callback = function(value) { booleanWidgetCallback(value, info.inputs[i].name.trim().toLowerCase()); };
           }
           /*
