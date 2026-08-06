859c4f5daf79ff5ff9164f66588947378bc580cd
ganbozza <ganbozza@gmail.com>
2026-08-06 14:23:13 +0800

    Update alegroupbypasser_service.js

--- web/alegroupbypasser_service.js
+++ web/alegroupbypasser_service.js
@@ -103,7 +103,7 @@ class AleGroupBypasserService {
         console.log("AleGroupBypasser_Service initialized...");
     }
 
-    syncNodesWidgetValue(ms=200) {
+    syncNodesWidgetValue(ms=500) {
         if(this._updatingWidget) return;
         setTimeout(() => {
             for (const node of this.nodes) {
