9155b1d2c6d4af73b2a107f254b23ce63918ac4f
ganbozza <ganbozza@gmail.com>
2026-08-06 20:43:35 +0800

    Update alegroupbypassernode.js

--- web/alegroupbypassernode.js
+++ web/alegroupbypassernode.js
@@ -94,10 +94,10 @@ function refreshWidgets(node) {
     }
 
     const group_alternate = parseSets(node.properties?.[ALTERNATE_KEY]  || "");
-
+    cons exclude_key = node.properties?.[EXCLUDE_KEY];
     for(const [key, val] of ALEGROUPBYPASSER_SERVICE.group_collections) {
         // skip exclude groups
-        if (new RegExp(node.properties?.[EXCLUDE_KEY], "i").exec(val?.title)) {
+        if (exclude_key && (new RegExp(exclude_key, "i").exec(val.title))) {
               continue;
           }
         if(!node.widgets || !node.widgets.find((w) => w.name === val.title)) {
