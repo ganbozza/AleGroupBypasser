52a53661de89cfb1a5c2bb2b7b93000cf58a9dc9
ganbozza <ganbozza@gmail.com>
2026-08-07 21:03:28 +0800

    Update alegroupbypassernode.js

--- web/alegroupbypassernode.js
+++ web/alegroupbypassernode.js
@@ -142,9 +142,10 @@ function refreshWidgets(node) {
         if(!node.widgets || !node.widgets.find((w) => w._hash_ref === gval.hashref)) {
             const boolWidget = addBooleanWidgetToNode(node, gval);
             const link_num = prev_inputs.find((p)=>p.widget.name===gval.title && p.widget._hash_ref===gval.hashref)?.link || null;
-            node.addInput(gval.title, "BOOLEAN", {link : link_num});
+            node.addInput(gval.title, "BOOLEAN");
             const slot = node.inputs.length-1;
             if(link_num!==null) {
+                node.inputs[slot].link = link_num;
                 node.graph.getLink(link_num).target_slot = slot;
             } else {
                 node.inputs[slot].widget = {  name : gval.title, _hash_ref : gval.hashref };
